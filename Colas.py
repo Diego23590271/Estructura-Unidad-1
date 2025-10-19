3e87dde5893165c63ac0667ffdf76ed78809facd
Diego Emiliano Moran Trejo <l23590271@sjuanrio.tecnm.mx>
2025-10-18 23:22:22 -0600

    Add files via upload

---
+++ Colas.py
@@ -0,0 +1,23 @@
+class Cola:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, dato):
+        self.items.append(dato)
+
+    def dequeue(self):
+        return self.items.pop(0) if not self.is_empty() else None
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+
+# Simulación
+cola_banco = Cola()
+cola_banco.enqueue("Ana")
+cola_banco.enqueue("Luis")
+cola_banco.enqueue("María")
+
+print("Atendiendo:", cola_banco.dequeue())  # Ana
+print("Atendiendo:", cola_banco.dequeue())  # Luis
+print("Atendiendo:", cola_banco.dequeue())  # María
